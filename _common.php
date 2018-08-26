4e66d9f09dc136929f4f6e291f0c73ce67ec6404
Mariusz Serek <mariusz@serek.net>
2018-08-26 13:03:02 +0200

    Add imports in _common.php, set default timezone when missing

--- _common.php
+++ _common.php
@@ -1,11 +1,15 @@
 <?php
     ini_set('display_errors', 'On');
+    if (!ini_get('date.timezone')) {
+        date_default_timezone_set('UTC');
+    }
     error_reporting(~E_DEPRECATED | E_STRICT);
 
     require_once realpath (dirname (__FILE__)) . '/classes/Config.class.php';
     require_once Config::$dir['CLASSES_DIRECTORY'] . 'Logger.class.php';
     require_once Config::$dir['CLASSES_DIRECTORY'] . 'Curl.class.php';
-    require_once Config::$dir['CLASSES_DIRECTORY'] . 'LiskAPI.class.php';
+    require_once Config::$dir['CLASSES_DIRECTORY'] . 'LiskAPI_0.class.php';
+    require_once Config::$dir['CLASSES_DIRECTORY'] . 'LiskAPI_1.class.php';
     require_once Config::$dir['CLASSES_DIRECTORY'] . 'AlwaysForge.class.php';
 
 ?>
